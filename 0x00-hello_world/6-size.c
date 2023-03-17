73a3d67d72056a746843de4f27fa4ae67b94d588
NeamaEssam <neama.essam20@gmail.com>
2023-03-16 19:49:19 -0700

    3rd commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
+* main - entry point
 *
-*description: program that prints the size of various types program that prints the size of various types
+* description: program that prints the size of various types program that prints the size of various types
 *
+* Return: Always 0 (Success) 
 */
 
 int main(void)
@@ -11,7 +13,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
