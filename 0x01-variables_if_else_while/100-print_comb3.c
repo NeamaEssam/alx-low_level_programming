893e363f6f3227e939d249a2f0ec48557236b595
NeamaEssam <neama.essam20@gmail.com>
2023-03-17 20:13:44 -0700

    2nd commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i != 9 && j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
