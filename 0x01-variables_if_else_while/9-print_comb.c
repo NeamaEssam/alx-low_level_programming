601612d3069491b1b38001b407f91eb7842ce91f
NeamaEssam <neama.essam20@gmail.com>
2023-03-17 19:44:27 -0700

    2nd commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			putchar(', ');
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
