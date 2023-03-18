7cdfb1421ed26bf1810e706292fb63df48642b0c
NeamaEssam <neama.essam20@gmail.com>
2023-03-17 18:30:40 -0700

    2nd commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char a;
+
+	a = 'a';
+	do {
+		putchar(a);
+		a++;
+	}
+	while (a <= 'z')
+
+	putchar('\n');
+	return (0);
+
+}
