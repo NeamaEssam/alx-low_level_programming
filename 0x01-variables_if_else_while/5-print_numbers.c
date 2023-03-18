72c6fa7440c99cb387ebe8e81a3e44b47033181d
NeamaEssam <neama.essam20@gmail.com>
2023-03-17 19:12:04 -0700

    2nd commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,13 +3,21 @@
 /**
  * main - Entry point
  *
- * Description: 
+ * Description: ptint numbers
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
+	int i;
 
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
 	return (0);
 }
