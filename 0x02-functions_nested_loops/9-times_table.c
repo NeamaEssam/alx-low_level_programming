9e81559e76605de2afa52f288c9dbd39e07ee2ff
NeamaEssam <neama.essam20@gmail.com>
2023-03-21 21:27:12 -0700

    1st commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+* times_table -  prints the 9 times table
+*
+* Return: Always 0.
+*/
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(i * j);
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
