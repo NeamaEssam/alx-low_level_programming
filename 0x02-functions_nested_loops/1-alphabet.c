33effc68ca5ef0668aef2b0279ec6ed420dd4bd5
NeamaEssam <neama.essam20@gmail.com>
2023-03-21 18:21:49 -0700

    1st commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include"main.h"
 #include<stdio.h>
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
 }
