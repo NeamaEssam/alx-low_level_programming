8ac633125b3a7bc9d4a20c5f9a92bb677b22617d
NeamaEssam <neama.essam20@gmail.com>
2023-03-21 19:22:00 -0700

    1st commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * print_sign - prints the sign of an integer
+ *
+ * @n: chacks the number for function
+ *
+ * Return: return 1 if +ve, 0 if zero, -1 if -ve
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
