97eb904e8b959e2d9b40fecc797d2f9de61325a5
NeamaEssam <neama.essam20@gmail.com>
2023-03-21 19:50:48 -0700

    1st commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+* print_last_digit - returns last digit
+*
+* @n: checks the num for func
+*
+* Return: return n
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
