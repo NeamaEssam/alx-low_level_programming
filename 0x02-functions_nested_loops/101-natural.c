284b87bcb0c7fbc0f9fcd658e2955a25f7b92c78
NeamaEssam <neama.essam20@gmail.com>
2023-03-22 20:03:00 -0700

    3rd commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+/**
+* main -   prints the n times table, starting with 0.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sum);
+return (0);
+}
