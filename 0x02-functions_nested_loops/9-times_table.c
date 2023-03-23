7214fb8ebdbe5737f8f50e1183bf3fb2c917b82e
NeamaEssam <neama.essam20@gmail.com>
2023-03-22 17:12:29 -0700

    2nd commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,18 +9,22 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, r;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar(48);
+		for (j = 1; j < 10; j++)
 		{
-			_putchar((i * j) + 48);
-			if (j != 9)
-			{
-				_putchar(',');
+			_putchar(',');
+			_putchar(' ');
+			r = i * j;
+			if (r <= 9)
 				_putchar(' ');
-			}
+			else
+				_putchar((r / 10) + 48);
+
+			_putchar((r / 10) + 48);
 		}
 		_putchar('\n');
 	}
