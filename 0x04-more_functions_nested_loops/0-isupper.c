1225849104ec2d0618106cf423cb5fdfbb80194a
NeamaEssam <neama.essam20@gmail.com>
2023-03-23 18:20:00 -0700

    1st commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+* main -   prints the n times table, starting with 0.
+* @c: input
+* Return: 1 if c is upper, 0 otherwise
+*/
+
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else if (c > 96 && c < 123)
+	{
+		return (0);
+	}
+}
