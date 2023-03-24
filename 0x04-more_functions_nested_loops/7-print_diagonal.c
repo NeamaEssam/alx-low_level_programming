a9da6041ccd11ea859d20930e1d3d949204a1b2d
NeamaEssam <neama.essam20@gmail.com>
2023-03-23 20:20:28 -0700

    1st commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+* print_diagonal - prints diagonal
+* @n: input
+* Return: always 0.
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		_putchar('\');
+		for (i = 1; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\');
+		}
+	}
+	_putchar('\n');
+}
