993467679d86098d1a40b9e9e1835c101b898d3e
NeamaEssam <neama.essam20@gmail.com>
2023-03-24 06:41:29 -0700

    1st commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,19 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		_putchar('\');
-		for (i = 1; i < n; i++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
-			_putchar('\');
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
