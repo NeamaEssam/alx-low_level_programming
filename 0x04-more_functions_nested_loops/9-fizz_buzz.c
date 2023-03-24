a118573fcea9a19e3fa558770933e2842b3e567d
NeamaEssam <neama.essam20@gmail.com>
2023-03-24 07:25:08 -0700

    1st commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,16 +11,16 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (!(i % 15))
+		if (i % 15 == 0)
 			printf("FizzBuzz ");
-		else if (!(i % 5))
+		else if (i % 5 == 0)
 		{
 			if (i == 100)
 				printf("Buzz");
 			else
 				printf("Buzz ");
 		}
-		else if (!(i % 3))
+		else if (i % 3 == 0)
 			printf("Fizz ");
 		else
 			printf("%d ", i);
