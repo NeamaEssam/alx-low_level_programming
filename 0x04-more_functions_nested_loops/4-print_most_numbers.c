7be59045257b745a9e3337d822a7c9dfc889fa87
NeamaEssam <neama.essam20@gmail.com>
2023-03-23 19:39:11 -0700

    1st commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+* print_most_numbers - prints the numbers, from 0 to 9 excipt 2 and 4
+* Return: always 0.
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		while (i != 2 || i != 4)
+			_putchar(i + 48);
+	}
+	_putchar('\n');
+}
