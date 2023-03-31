d0bf440d7c899a35d98babd750b80996fd976797
NeamaEssam <neama.essam20@gmail.com>
2023-03-30 17:51:15 -0700

    1st commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,7 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int n1, n2, i;
-
-	n1 = 0;
-	while (dest[n1])
-		n1++;
-
-	n2 = 0;
-	while (src[n2])
-		n2++;
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
