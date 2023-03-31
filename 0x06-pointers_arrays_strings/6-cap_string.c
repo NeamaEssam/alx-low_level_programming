48d783f1ed21d18b90c20d62c3fa5eb8c2b1cb98
NeamaEssam <neama.essam20@gmail.com>
2023-03-30 20:07:22 -0700

    1st commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,57 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+* islower - checks if lowercase
+* @b: input
+* Return: 1 if true, 0 if false
+*/
+
+int islower(char b)
+{
+	return (b >= 97 && b <= 122);
+}
+
+/**
+* separator - checks if separator char
+* @c: input
+* Return: 1 if true, 0 if false
+*/
+
+int separator(char c)
+{
+	int i;
+	char s[] = "\t\n,.!?\"(){}";
+
+	for (i = 0; i <= 11; i++)
+		if (c == s[i])
+			return (1);
+	return (0);
+}
+
+/**
+* cap_string - capitalize all chars of string
+* @a: input
+* Return: string
+*/
+
+char *cap_string(char *a)
+{
+	char *p = a;
+	int s = 1;
+
+	while (*a)
+	{
+		if (separator(*a))
+			s = 1;
+		else if (islower(*a) && separator)
+		{
+			*a -= 32;
+			s = 0;
+		}
+		else
+			s = 0;
+		a++;
+	}
+	return (p);
+}
