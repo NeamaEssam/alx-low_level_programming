9d8edd958fb96cd9e5c1d4dca2d9b2cb07fe7b8e
NeamaEssam <neama.essam20@gmail.com>
2023-04-03 19:29:53 -0700

    3rd commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,8 +11,7 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i;
-	int n1, n2, j;
+	unsigned int i, n1, n2, j;
 
 	n1 = 0;
 	while (s[n1])
@@ -21,6 +20,7 @@ unsigned int _strspn(char *s, char *accept)
 	n2 = 0;
 	while (accept[n2])
 		n2++;
+
 	for (i = 0; i < n1; i++)
 	{
 		for (j = 0; accept[j] != s[i]; j++)
