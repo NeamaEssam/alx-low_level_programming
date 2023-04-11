29a8659a53c2f16f4cb7567c0e1fa937e1c7ace8
NeamaEssam <neama.essam20@gmail.com>
2023-04-11 12:12:20 -0700

    3rd commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - main function
+* @argc: int
+* @argv: list
+* Return: always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0, num;
+
+	if (argc >= 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			num = atoi(argv[i]);
+			if (num == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum += num;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
