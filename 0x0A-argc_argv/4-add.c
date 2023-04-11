ea2aa6aad3f678f22521ced5c08066eec365a015
NeamaEssam <neama.essam20@gmail.com>
2023-04-11 12:23:47 -0700

    3rd commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,13 +11,15 @@
 int main(int argc, char *argv[])
 {
 	int i, sum = 0, num;
+	char *c;
 
 	if (argc >= 2)
 	{
 		for (i = 1; i < argc; i++)
 		{
+			c = argv[i];
 			num = atoi(argv[i]);
-			if (*argv[i] < '0' || *argv[i] > '9')
+			if (*c < '0' || *c > '9')
 			{
 				printf("Error\n");
 				return (1);
