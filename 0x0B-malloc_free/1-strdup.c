45da1bcbd0b7eb6e8b3deb406477214869d1d8b0
NeamaEssam <neama.essam20@gmail.com>
2023-04-14 11:48:58 -0700

    1st commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,21 +14,21 @@ char *_strdup(char *str)
 	unsigned int i, n;
 
 	n = 0;
-	while(str[n])
+	while (str[n])
 		n++;
 
 	a = malloc(n + 1);
-	if (str == NULL)
+	if (str == Null || a == 0)
 	{
 		return ('\0');
 	}
-	else
+
+
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			a[i] = str[i];
-		}
+		a[i] = str[i];
 	}
+
 	return (a);
 	free(a);
 }
