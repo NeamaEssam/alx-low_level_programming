4bc83fa12a85733f60185de387890cc1e2bb7f14
NeamaEssam <neama.essam20@gmail.com>
2023-04-14 12:20:28 -0700

    1st commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include"main.h"
+#include<stdlib.h>
+
+/**
+* str_concat - concatenates two strings.
+* @s1: input
+* @s2: input
+* Return: pointer to string
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	unsigned int i, n1, n2;
+
+	if (str == NULL)
+	{
+		return ('\0');
+	}
+	n1 = 0;
+	while (s1[n1])
+		n1++;
+
+	n2 = 0;
+	while (s2[n2])
+		n2++;
+
+	a = malloc(n1 + n2 + 1);
+	if (a == 0)
+	{
+		return ('\0');
+	}
+	for (i = 0; i < n1; i++)
+	{
+		a[i] = s1[i];
+	}
+	for (; i < n1 + n2; i++)
+	{
+		a[i] = s2[i];
+	}
+
+	return (a);
+	free(a);
+}
