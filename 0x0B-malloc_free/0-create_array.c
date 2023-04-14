cb13cbd7eb71b3342b5226c0c87aa386c69d812b
NeamaEssam <neama.essam20@gmail.com>
2023-04-14 10:56:25 -0700

    1st commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,13 @@ char *create_array(unsigned int size, char c)
 	char *a;
 	unsigned int i;
 
-	if (size == 0)
+	a = malloc(size * sizeof(char));
+	if (size == 0 || a == 0)
 	{
 		return ('\0');
 	}
 	else
 	{
-		*a = malloc(size * sizeof(char));
 		for (i = 0; i < size; i++)
 		{
 			a[i] = c;
