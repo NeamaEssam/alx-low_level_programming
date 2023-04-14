3bdae33eed016acc862c405c11b8375edf49074c
NeamaEssam <neama.essam20@gmail.com>
2023-04-14 12:50:14 -0700

    1st commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,27 +14,27 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, n1, n2;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
-	n1 = 1;
+		s2 = "";
+	n1 = 0;
 	while (s1[n1])
 		n1++;
 
-	n2 = 1;
+	n2 = 0;
 	while (s2[n2])
 		n2++;
 
-	a = malloc(n1 + n2 - 1);
+	a = malloc(n1 + n2 + 1);
 	if (a == 0)
 	{
 		return ('\0');
 	}
-	for (i = 0; i <= n1; i++)
+	for (i = 0; i < n1; i++)
 	{
 		a[i] = s1[i];
 	}
-	for (; i <= (n1 + n2 - 1); i++)
+	for (; i < (n1 + n2); i++)
 	{
 		a[i] = s2[i - n1];
 	}
