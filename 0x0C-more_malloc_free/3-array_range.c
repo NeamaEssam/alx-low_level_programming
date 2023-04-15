43106e5cf503fb0e0164f32fae38b6f8488b72f7
NeamaEssam <neama.essam20@gmail.com>
2023-04-14 19:13:20 -0700

    1st commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,16 +18,17 @@ int *array_range(int min, int max)
 		return ('\0');
 	}
 
-	//size = (max - min) + 1;
+	/*size = (max - min) + 1;*/
 	a = malloc((max - min + 1) * sizeof(int));
 
 	if (a == 0)
 		return ('\0');
 
-	for (i = 0; min <= max; i++)
+	while (min <= max)
 	{
 		a[i] = min;
 		min++;
+		i++;
 	}
 	return (a);
 	free(a);
