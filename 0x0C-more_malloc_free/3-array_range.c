74797d248305b48d6228c20caa903dd61ff20c5e
NeamaEssam <neama.essam20@gmail.com>
2023-04-14 19:22:48 -0700

    1st commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,23 +8,47 @@
 * Return: pointer
 */
 
+/*int *array_range(int min, int max)
+*{
+*	char *a;
+*	int i = 0;
+*
+*	if (min > max)
+*	{
+*		return ('\0');
+*	}
+*
+*
+*	a = malloc((max - min + 1) * sizeof(int));
+*
+*	if (a == 0)
+*		return ('\0');
+*
+*	while (min <= max)
+*	{
+*		a[i] = min;
+*		min++;
+*		i++;
+*	}
+*	return (a);
+}*/
+
 int *array_range(int min, int max)
 {
-	char *a;
-	int i = 0;
+	int *a;
+	int i;
 
 	if (min > max)
 	{
 		return ('\0');
 	}
 
-	/*size = (max - min) + 1;*/
-
-	a = malloc((max - min + 1) * sizeof(int));
-
+	a = malloc(sizeof(int) * (max - min + 1));
 	if (a == 0)
+	{
 		return ('\0');
-
+	}
+	i = 0;
 	while (min <= max)
 	{
 		a[i] = min;
@@ -32,5 +56,4 @@ int *array_range(int min, int max)
 		i++;
 	}
 	return (a);
-
 }
