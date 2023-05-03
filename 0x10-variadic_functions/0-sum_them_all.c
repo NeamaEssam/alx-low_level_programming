8c83a229c20db32f28926179250cde4a451df955
NeamaEssam <neama.essam20@gmail.com>
2023-05-03 12:22:17 -0700

    1st commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,17 +8,13 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
-
 	va_list numbers;
+	unsigned int sum = 0, i;
 
 	va_start(numbers, n);
 
-	int sum = 0, i;
-
 	for (i = 0; i < n; i++)
-		sum += va_arg(numbers);
+		sum += va_arg(numbers, int);
 
 	va_end(numbers);
 	return (sum);
