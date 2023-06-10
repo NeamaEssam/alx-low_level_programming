f143a60eab957c0631d5c10bbed076bc35ee3c6a
NeamaEssam <neama.essam20@gmail.com>
2023-06-09 17:16:51 -0700

    1st commit

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+* _abs - computes the abs value
+*
+* @n: checks the num for func
+*
+* Return: return n
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = -1 * n;
+	return (n);
+}
