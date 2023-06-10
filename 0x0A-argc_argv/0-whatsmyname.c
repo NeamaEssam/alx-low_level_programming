448d1aecce7378484ad16c9df82beac2ff684817
NeamaEssam <neama.essam20@gmail.com>
2023-06-09 17:37:48 -0700

    1st commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - main function
+* @argc: int
+* @argv: list
+* Return: always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
