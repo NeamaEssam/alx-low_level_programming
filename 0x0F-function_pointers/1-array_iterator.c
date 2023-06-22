8e0b918782c75d2914e686ead3202195e55b1bc3
NeamaEssam <neama.essam20@gmail.com>
2023-06-21 17:38:52 -0700

    1st commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include"function_pointers.h"
+#include<stdio.h>
+
+/**
+* array_iterator - prints a name.
+* @array: input
+* @size: input
+* @action: input
+* Return: always 0
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *last;
+
+	last = array + size - 1;
+
+	if (array && size && action)
+	{
+		while (array <= last)
+		{
+			action(*array++);
+		}
+	}
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include"3-calc.h"
+
+/**
+* op_add - searches for an integer.
+* @a: input
+* @b: input
+* Return: sum of a, b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - subtract a, b
+* @a: input
+* @b: input
+* Return: sum of a, b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - multiply a, b
+* @a: input
+* @b: input
+* Return: mul of a, b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - division a, b
+* @a: input
+* @b: input
+* Return: div of a, b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - modulas a, b
+* @a: input
+* @b: input
+* Return: mod of a, b
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
