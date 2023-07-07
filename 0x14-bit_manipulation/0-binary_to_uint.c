29bf8648cbd25c179eb680ce3ae7dfbe1191dd86
NeamaEssam <neama.essam20@gmail.com>
2023-07-06 19:29:38 -0700

    1st commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * binary_to_uint -  converts a binary number to an unsigned int.
+ * @b: const
+ * Return: converted num.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+
+}
