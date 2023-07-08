88bd5fb229a0f303648c77aa732a3b5bc11351c1
NeamaEssam <neama.essam20@gmail.com>
2023-07-07 19:02:32 -0700

    1st commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,6 +8,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int x = 0;
 
+	if (!b)
+		return 0;
 
+	for (x = 0; *b; b++) 
+	{
+		if (*b != '0' && *b != '1')
+			return 0;
+		else if (*b == '1')
+			x = (x << 1) | 1;
+		else if (*b == '0' )
+			x <<= 1;
+		else
+			break;
+	}
+
+	return x;
 }
