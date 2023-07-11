0a3a179af40695921c2b0d7e692ec2dfd8e1e801
NeamaEssam <neama.essam20@gmail.com>
2023-07-10 18:51:17 -0700

    2nd commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * _strlen - return string length
+ * @s: string to be used
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	if (!s)
+		return (0);
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: input
+ * @text_content: input
+ * Return: 1 on success, -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t n = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		n = write(file, text_content, len);
+		if (n == -1)
+		{
+			close(file);
+			return (-1);
+		}
+		close(file);
+		return (1);
+	}
+	else
+	{
+		close(file);
+		return (1);
+	}
+}
